efe25d84ec8d235864ef74ee8ce8b7d75502e08d
Luca Casonato <lucacasonato@yahoo.com>
2020-07-10 22:42:09 +0200

    Verify that requests actually come from GitHub

---
+++ test_deps.ts
@@ -0,0 +1,5 @@
+export {
+  assert,
+  assertEquals,
+  assertThrows,
+} from "https://deno.land/std@0.60.0/testing/asserts.ts";
---
+++ utils/net_test.ts
@@ -0,0 +1,23 @@
+import { assert, assertEquals, assertThrows } from "../test_deps.ts";
+import { isIp4InCidr, ip4ToInt } from "./net.ts";
+
+Deno.test({
+  name: "ipv4 parsing",
+  fn() {
+    assert(ip4ToInt("1.1.1.1"));
+    assertThrows(() => ip4ToInt("1.1.1.1.1"));
+    assertThrows(() => ip4ToInt("1.1.1.-1"));
+    assertThrows(() => ip4ToInt("1.1.1.300"));
+  },
+});
+
+Deno.test({
+  name: "ipv4 in cidr matches",
+  fn() {
+    assertEquals(isIp4InCidr("1.1.1.1")("0.0.0.0/0"), true);
+    assertEquals(isIp4InCidr("1.1.1.1")("1.1.1.0/24"), true);
+    assertEquals(isIp4InCidr("1.1.1.1")("1.1.1.0/31"), true);
+    assertEquals(isIp4InCidr("1.1.1.1")("1.1.1.0/32"), false);
+    assertEquals(isIp4InCidr("1.1.1.1")("1.2.1.0/31"), false);
+  },
+});
