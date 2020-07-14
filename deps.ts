b1237943facdd85486f9e35d441419185ab7e024
Luca Casonato <luca.casonato@antipy.com>
2020-07-14 17:38:30 +0200

    Update Deno to 1.2.0 (#9)

--- deps.ts
+++ deps.ts
@@ -4,12 +4,12 @@ export {
   expandGlob,
   walk,
   readJsonSync,
-} from "https://deno.land/std@0.58.0/fs/mod.ts";
-export { join } from "https://deno.land/std@0.58.0/path/mod.ts";
+} from "https://deno.land/std@v0.61.0/fs/mod.ts";
+export { join } from "https://deno.land/std@v0.61.0/path/mod.ts";
 export { S3Client } from "https://raw.githubusercontent.com/lucacasonato/deno_s3/5d80dc7d60abd114b0d900fb7f03857faa942258/mod.ts";
 export { prettyBytes } from "https://raw.githubusercontent.com/brunnerlivio/deno-pretty-bytes/master/mod.ts";
 export { createClient } from "https://denopkg.com/chiefbiiko/dynamodb@v1.0.0/mod.ts";
-export * as YAML from "https://deno.land/std@0.58.0/encoding/yaml.ts";
+export * as YAML from "https://deno.land/std@v0.61.0/encoding/yaml.ts";
 export {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
