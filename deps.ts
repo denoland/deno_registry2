17fd49f55b1d3374cf87b33649c9a09a70ed2133
Luca Casonato <lucacasonato@yahoo.com>
2020-07-14 19:50:22 +0200

    Update deno_mongo_lambda

--- deps.ts
+++ deps.ts
@@ -6,12 +6,12 @@ export {
   readJsonSync,
 } from "https://deno.land/std@v0.61.0/fs/mod.ts";
 export { join } from "https://deno.land/std@v0.61.0/path/mod.ts";
-export { S3Client } from "https://raw.githubusercontent.com/lucacasonato/deno_s3/5d80dc7d60abd114b0d900fb7f03857faa942258/mod.ts";
+export { S3Client } from "https://raw.githubusercontent.com/lucacasonato/deno_s3/52cfca3d40e49d6da0ebcc91fc5af18e4b709a69/mod.ts";
 export { prettyBytes } from "https://raw.githubusercontent.com/brunnerlivio/deno-pretty-bytes/master/mod.ts";
 export * as YAML from "https://deno.land/std@v0.61.0/encoding/yaml.ts";
-export {
+export type {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
   Context,
-} from "https://deno.land/x/lambda/mod.ts";
-export { MongoClient } from "https://raw.githubusercontent.com/lucacasonato/deno_mongo_lambda/v0.9.0-dev1/mod.ts";
+} from "https://deno.land/x/lambda/types.d.ts";
+export { MongoClient } from "https://raw.githubusercontent.com/lucacasonato/deno_mongo_lambda/v0.9.0-dev2/mod.ts";
