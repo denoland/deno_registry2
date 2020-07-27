171d044908d0993596334bcd2526766102361e4e
Luca Casonato <lucacasonato@yahoo.com>
2020-07-27 20:34:41 +0200

    Clean checkout more agressively

--- api/async/publish.ts
+++ api/async/publish.ts
@@ -73,122 +73,124 @@ async function publishGithub(
   // Clone the repository from GitHub
   const cloneURL = `https://github.com/${repository}`;
   const clonePath = await clone(cloneURL, ref);
-
+  
   console.log("Finished clone");
 
-  // Upload files to S3
-  const skippedFiles: string[] = [];
-  const pendingUploads: Promise<void>[] = [];
-  const directory: DirectoryListingFile[] = [];
+  try {
+    // Upload files to S3
+    const skippedFiles: string[] = [];
+    const pendingUploads: Promise<void>[] = [];
+    const directory: DirectoryListingFile[] = [];
 
-  // Create path that has possible subdir prefix
-  const path = (subdir === undefined ? clonePath : join(clonePath, subdir))
-    .replace(
-      /\/$/,
-      "",
-    );
+    // Create path that has possible subdir prefix
+    const path = (subdir === undefined ? clonePath : join(clonePath, subdir))
+      .replace(
+        /\/$/,
+        "",
+      );
 
-  // Walk all files in the repository (that start with the subdir if present)
-  const entries = [];
-  for await (
-    const entry of walk(path, {
-      includeFiles: true,
-      includeDirs: true,
-    })
-  ) {
-    entries.push(entry);
-  }
-
-  console.log("Total files in repo", entries.length);
-
-  // Pool requests because of https://github.com/denoland/deno_registry2/issues/15
-  await asyncPool(95, entries, async (entry) => {
-    // If this is a file in the .git folder, ignore it
-    if (
-      entry.path.startsWith(join(path, ".git/")) ||
-      entry.path === join(path, ".git")
+    // Walk all files in the repository (that start with the subdir if present)
+    const entries = [];
+    for await (
+      const entry of walk(path, {
+        includeFiles: true,
+        includeDirs: true,
+      })
     ) {
-      return;
+      entries.push(entry);
     }
 
-    const filename = entry.path.substring(path.length);
-    if (entry.isFile) {
-      const file = await Deno.open(entry.path);
-      const body = await Deno.readAll(file);
-      if (body.length > MAX_FILE_SIZE) {
-        skippedFiles.push(filename);
+    console.log("Total files in repo", entries.length);
+
+    // Pool requests because of https://github.com/denoland/deno_registry2/issues/15
+    await asyncPool(95, entries, async (entry) => {
+      // If this is a file in the .git folder, ignore it
+      if (
+        entry.path.startsWith(join(path, ".git/")) ||
+        entry.path === join(path, ".git")
+      ) {
         return;
       }
-      directory.push(
-        { path: filename, size: body.length, type: "file" },
-      );
-      await uploadVersionRaw(
-        moduleName,
-        version,
-        filename,
-        body,
-      );
-      file.close();
-    } else {
-      directory.push({ path: filename, size: undefined, type: "dir" });
-    }
-  });
 
-  const versionsBody = await getMeta(moduleName, "versions.json");
-  const versions = versionsBody
-    ? JSON.parse(decoder.decode(versionsBody))
-    : { versions: [] };
-  await uploadMeta(
-    moduleName,
-    "versions.json",
-    encoder.encode(
-      JSON.stringify(
-        { latest: version, versions: [version, ...versions.versions] },
+      const filename = entry.path.substring(path.length);
+      if (entry.isFile) {
+        const file = await Deno.open(entry.path);
+        const body = await Deno.readAll(file);
+        if (body.length > MAX_FILE_SIZE) {
+          skippedFiles.push(filename);
+          return;
+        }
+        directory.push(
+          { path: filename, size: body.length, type: "file" },
+        );
+        await uploadVersionRaw(
+          moduleName,
+          version,
+          filename,
+          body,
+        );
+        file.close();
+      } else {
+        directory.push({ path: filename, size: undefined, type: "dir" });
+      }
+    });
+
+    const versionsBody = await getMeta(moduleName, "versions.json");
+    const versions = versionsBody
+      ? JSON.parse(decoder.decode(versionsBody))
+      : { versions: [] };
+    await uploadMeta(
+      moduleName,
+      "versions.json",
+      encoder.encode(
+        JSON.stringify(
+          { latest: version, versions: [version, ...versions.versions] },
+        ),
       ),
-    ),
-  );
+    );
 
-  // Remove checkout
-  await Deno.remove(clonePath, { recursive: true });
-
-  // Calculate directory sizes
-  // TODO: make more efficient
-  for (const entry of directory) {
-    if (entry.type === "dir") {
-      entry.size = 0;
-      for (
-        const f of directory.filter(
-          (f) => f.type === "file" && f.path.startsWith(entry.path),
-        )
-      ) {
-        entry.size += f.size ?? 0;
+    // Calculate directory sizes
+    // TODO: make more efficient
+    for (const entry of directory) {
+      if (entry.type === "dir") {
+        entry.size = 0;
+        for (
+          const f of directory.filter(
+            (f) => f.type === "file" && f.path.startsWith(entry.path),
+          )
+        ) {
+          entry.size += f.size ?? 0;
+        }
       }
     }
+
+    // Upload directory listing to S3
+    await uploadVersionMeta(
+      moduleName,
+      version,
+      "meta.json",
+      encoder.encode(JSON.stringify({
+        uploaded_at: new Date().toISOString(),
+        directory_listing: directory,
+        upload_options: {
+          type: "github",
+          repository,
+          subdir,
+          ref,
+        },
+      })),
+    );
+
+    await database.saveBuild({
+      ...build,
+      status: "success",
+      message:
+        `Uploaded ${pendingUploads.length} files. Skipped files due to size: ${
+          JSON.stringify(skippedFiles)
+        }`,
+    });
+  } finally {
+    // Remove checkout
+    await Deno.remove(clonePath, { recursive: true });
   }
-
-  // Upload directory listing to S3
-  await uploadVersionMeta(
-    moduleName,
-    version,
-    "meta.json",
-    encoder.encode(JSON.stringify({
-      uploaded_at: new Date().toISOString(),
-      directory_listing: directory,
-      upload_options: {
-        type: "github",
-        repository,
-        subdir,
-        ref,
-      },
-    })),
-  );
-
-  await database.saveBuild({
-    ...build,
-    status: "success",
-    message:
-      `Uploaded ${pendingUploads.length} files. Skipped files due to size: ${
-        JSON.stringify(skippedFiles)
-      }`,
-  });
 }
