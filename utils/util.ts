b575510aff8d117cd1e2d250be8491df041cac1c
Luca Casonato <lucacasonato@yahoo.com>
2020-07-27 18:37:26 +0200

    Fix types

--- utils/util.ts
+++ utils/util.ts
@@ -1,17 +1,18 @@
 // From https://github.com/rxaviers/async-pool/blob/master/lib/es7.js.
 // Copyright (c) 2017 Rafael Xavier de Souza http://rafael.xavier.blog.br
 // Licensed under MIT
+
 export async function asyncPool<T, R>(
   poolLimit: number,
   array: T[],
   iteratorFn: (data: T, array: T[]) => Promise<R>,
 ): Promise<R[]> {
   const ret: Promise<R>[] = [];
-  const executing: Promise<any>[] = [];
+  const executing: Promise<unknown>[] = [];
   for (const item of array) {
     const p = Promise.resolve().then(() => iteratorFn(item, array));
     ret.push(p);
-    const e: Promise<any> = p.then(() =>
+    const e: Promise<unknown> = p.then(() =>
       executing.splice(executing.indexOf(e), 1)
     );
     executing.push(e);
