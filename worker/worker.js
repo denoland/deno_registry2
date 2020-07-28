cc7bb1f4ca7abc10c5acea71d11d5193355ddfc1
Luca Casonato <lucacasonato@yahoo.com>
2020-07-28 13:24:35 +0200

    Add caching to worker

--- worker/worker.js
+++ worker/worker.js
@@ -7,7 +7,9 @@ const S3_BUCKET =
  */
 async function handleRequest(request) {
   const url = new URL(request.url);
-  const resp = await fetch(`${S3_BUCKET}${url.pathname}`);
+  const resp = await fetch(`${S3_BUCKET}${url.pathname}`, {
+    cf: { cacheEverything: true },
+  });
   const resp2 = new Response(resp.body, resp);
   resp2.headers.set("Access-Control-Allow-Origin", "*");
   return resp2;
