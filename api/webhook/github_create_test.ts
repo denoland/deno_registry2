1e597bc977278753f6fc7c94612aafab91a83199
Luca Casonato <lucacasonato@yahoo.com>
2020-08-06 22:16:36 +0200

    added final create test

--- api/webhook/github_create_test.ts
+++ api/webhook/github_create_test.ts
@@ -5,12 +5,11 @@ import {
 } from "../../utils/test_utils.ts";
 import { Database } from "../../utils/database.ts";
 import { assertEquals, readJson } from "../../test_deps.ts";
-import { getMeta, s3 } from "../../utils/storage.ts";
+import { getMeta, s3, uploadMetaJson } from "../../utils/storage.ts";
 const database = new Database(Deno.env.get("MONGO_URI")!);
 
 const decoder = new TextDecoder();
 
-const pingevent = await readJson("./api/webhook/testdata/pingevent.json");
 const createevent = await readJson("./api/webhook/testdata/createevent.json");
 const createeventBranch = await readJson(
   "./api/webhook/testdata/createevent_branch.json",
@@ -18,184 +17,6 @@ const createeventBranch = await readJson(
 const createeventVersionPrefix = await readJson(
   "./api/webhook/testdata/createevent_versionprefix.json",
 );
-
-Deno.test({
-  name: "ping event no name",
-  async fn() {
-    // Send ping event
-    const resp = await handler(
-      createJSONWebhookEvent(
-        "ping",
-        "/webhook/gh/",
-        pingevent,
-        { name: "" },
-        {},
-      ),
-      createContext(),
-    );
-    assertEquals(resp, {
-      body: '{"success":false,"error":"no module name specified"}',
-
-      headers: {
-        "content-type": "application/json",
-      },
-      statusCode: 400,
-    });
-
-    // Check that no versions.json file exists
-    assertEquals(await getMeta("ltest-2", "versions.json"), undefined);
-
-    // Check that no builds are queued
-    assertEquals(await database._builds.find({}), []);
-
-    // Check that there is no module entry in the database
-    assertEquals(await database.getModule("ltest-2"), null);
-  },
-});
-
-Deno.test({
-  name: "ping event bad name",
-  async fn() {
-    // Send ping event
-    const resp = await handler(
-      createJSONWebhookEvent(
-        "ping",
-        "/webhook/gh/ltest-2",
-        pingevent,
-        { name: "ltest-2" },
-        {},
-      ),
-      createContext(),
-    );
-    assertEquals(resp, {
-      body: '{"success":false,"error":"module name is not valid"}',
-      headers: {
-        "content-type": "application/json",
-      },
-      statusCode: 400,
-    });
-
-    // Check that no versions.json file exists
-    assertEquals(await getMeta("ltest-2", "versions.json"), undefined);
-
-    // Check that no builds are queued
-    assertEquals(await database._builds.find({}), []);
-
-    // Check that there is no module entry in the database
-    assertEquals(await database.getModule("ltest-2"), null);
-  },
-});
-
-Deno.test({
-  name: "ping event success",
-  async fn() {
-    // Send ping event
-    const resp = await handler(
-      createJSONWebhookEvent(
-        "ping",
-        "/webhook/gh/ltest2",
-        pingevent,
-        { name: "ltest2" },
-        {},
-      ),
-      createContext(),
-    );
-    assertEquals(resp, {
-      body:
-        '{"success":true,"data":{"module":"ltest2","repository":"luca-rand/testing"}}',
-      headers: {
-        "content-type": "application/json",
-      },
-      statusCode: 200,
-    });
-
-    // Check that the database entry
-    assertEquals(
-      await database.getModule("ltest2"),
-      {
-        name: "ltest2",
-        type: "github",
-        repository: "luca-rand/testing",
-        description: "Move along, just for testing",
-        star_count: 2,
-      },
-    );
-
-    // Check that a versions.json file was created
-    assertEquals(
-      JSON.parse(decoder.decode(await getMeta("ltest2", "versions.json"))),
-      { latest: null, versions: [] },
-    );
-
-    // Check that no new build was queued
-    assertEquals(await database._builds.find({}), []);
-
-    // Clean up
-    await s3.deleteObject("ltest2/meta/versions.json");
-    await database._modules.deleteMany({});
-  },
-});
-
-Deno.test({
-  name: "ping event max registered to repository",
-  async fn() {
-    await database.saveModule({
-      name: "ltest2",
-      type: "github",
-      repository: "luca-rand/testing",
-      description: "",
-      star_count: 4,
-    });
-    await database.saveModule({
-      name: "ltest3",
-      type: "github",
-      repository: "luca-rand/testing",
-      description: "",
-      star_count: 4,
-    });
-    await database.saveModule({
-      name: "ltest4",
-      type: "github",
-      repository: "luca-rand/testing",
-      description: "",
-      star_count: 4,
-    });
-
-    // Send ping event for ltest5
-    assertEquals(
-      await handler(
-        createJSONWebhookEvent(
-          "ping",
-          "/webhook/gh/ltest5",
-          pingevent,
-          { name: "ltest5" },
-          {},
-        ),
-        createContext(),
-      ),
-      {
-        body:
-          '{"success":false,"error":"max number of modules for one repository (3) has been reached"}',
-        headers: {
-          "content-type": "application/json",
-        },
-        statusCode: 400,
-      },
-    );
-
-    // Check that no versions.json file exists
-    assertEquals(await getMeta("ltest5", "versions.json"), undefined);
-
-    // Check that there is no module entry in the database
-    assertEquals(await database.getModule("ltest5"), null);
-
-    // Check that builds were queued
-    assertEquals(await database._builds.find({}), []);
-
-    // Clean up
-    await database._modules.deleteMany({});
-  },
-});
 
 Deno.test({
   name: "create event no name",
@@ -496,17 +317,14 @@ Deno.test({
       statusCode: 200,
     });
 
-    // Check that the database entry
-    assertEquals(
-      await database.getModule("ltest2"),
-      {
-        name: "ltest2",
-        type: "github",
-        repository: "luca-rand/testing",
-        description: "Move along, just for testing",
-        star_count: 2,
-      },
-    );
+    // Check that the database entry was created
+    assertEquals(await database.getModule("ltest2"), {
+      name: "ltest2",
+      type: "github",
+      repository: "luca-rand/testing",
+      description: "Move along, just for testing",
+      star_count: 2,
+    });
 
     // Check that no versions.json file was created
     assertEquals(await getMeta("ltest2", "versions.json"), undefined);
@@ -622,23 +440,72 @@ Deno.test({
       statusCode: 200,
     });
 
-    // Check that the database entry
-    assertEquals(
-      await database.getModule("ltest2"),
-      {
-        name: "ltest2",
-        type: "github",
-        repository: "luca-rand/testing",
-        description: "Move along, just for testing",
-        star_count: 2,
-      },
-    );
+    // Check that the database entry was created
+    assertEquals(await database.getModule("ltest2"), {
+      name: "ltest2",
+      type: "github",
+      repository: "luca-rand/testing",
+      description: "Move along, just for testing",
+      star_count: 2,
+    });
 
     // Check that no versions.json file was created
     assertEquals(await getMeta("ltest2", "versions.json"), undefined);
 
     // Clean up
     await database._builds.deleteMany({});
+    await database._modules.deleteMany({});
+  },
+});
+
+Deno.test({
+  name: "create event already exists",
+  async fn() {
+    await uploadMetaJson(
+      "ltest2",
+      "versions.json",
+      { latest: "0.0.7", versions: ["0.0.7"] },
+    );
+
+    // Send create event
+    const resp = await handler(
+      createJSONWebhookEvent(
+        "create",
+        "/webhook/gh/ltest2",
+        createevent,
+        { name: "ltest2" },
+        {},
+      ),
+      createContext(),
+    );
+    assertEquals(resp, {
+      body: '{"success":false,"error":"version already exists"}',
+      headers: {
+        "content-type": "application/json",
+      },
+      statusCode: 400,
+    });
+
+    // Check that the database entry was created
+    assertEquals(await database.getModule("ltest2"), {
+      name: "ltest2",
+      type: "github",
+      repository: "luca-rand/testing",
+      description: "Move along, just for testing",
+      star_count: 2,
+    });
+
+    // Check that versions.json was not changed
+    assertEquals(
+      JSON.parse(decoder.decode(await getMeta("ltest2", "versions.json"))),
+      { latest: "0.0.7", versions: ["0.0.7"] },
+    );
+
+    // Check that no new build was queued
+    assertEquals(await database._builds.find({}), []);
+
+    // Clean up
+    await s3.deleteObject("ltest2/meta/versions.json");
     await database._modules.deleteMany({});
   },
 });
