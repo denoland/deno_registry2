d94996c670d1104fa1f19b0f52835401019afaeb
William Perron <william.perron@live.ca>
2020-08-09 11:39:57 -0400

    feat: update webhook to accept x-www-form-urlencoded requests (#54)

--- api/webhook/github_ping_test.ts
+++ api/webhook/github_ping_test.ts
@@ -1,6 +1,7 @@
 import { handler } from "./github.ts";
 import {
   createJSONWebhookEvent,
+  createJSONWebhookWebFormEvent,
   createContext,
 } from "../../utils/test_utils.ts";
 import { Database } from "../../utils/database.ts";
@@ -11,6 +12,9 @@ const database = new Database(Deno.env.get("MONGO_URI")!);
 const decoder = new TextDecoder();
 
 const pingevent = await readJson("./api/webhook/testdata/pingevent.json");
+const urlendodedpingevent = await Deno.readTextFile(
+  "./api/webhook/testdata/pingevent.txt",
+);
 
 Deno.test({
   name: "ping event no name",
@@ -88,6 +92,56 @@ Deno.test({
         "ping",
         "/webhook/gh/ltest2",
         pingevent,
+        { name: "ltest2" },
+        {},
+      ),
+      createContext(),
+    );
+    assertEquals(resp, {
+      body:
+        '{"success":true,"data":{"module":"ltest2","repository":"luca-rand/testing"}}',
+      headers: {
+        "content-type": "application/json",
+      },
+      statusCode: 200,
+    });
+
+    // Check that the database entry
+    assertEquals(
+      await database.getModule("ltest2"),
+      {
+        name: "ltest2",
+        type: "github",
+        repository: "luca-rand/testing",
+        description: "Move along, just for testing",
+        star_count: 2,
+      },
+    );
+
+    // Check that a versions.json file was created
+    assertEquals(
+      JSON.parse(decoder.decode(await getMeta("ltest2", "versions.json"))),
+      { latest: null, versions: [] },
+    );
+
+    // Check that no new build was queued
+    assertEquals(await database._builds.find({}), []);
+
+    // Clean up
+    await s3.deleteObject("ltest2/meta/versions.json");
+    await database._modules.deleteMany({});
+  },
+});
+
+Deno.test({
+  name: "ping event success - web form",
+  async fn() {
+    // Send ping event
+    const resp = await handler(
+      createJSONWebhookWebFormEvent(
+        "ping",
+        "/webhook/gh/ltest2",
+        btoa(urlendodedpingevent),
         { name: "ltest2" },
         {},
       ),
--- utils/test_utils.ts
+++ utils/test_utils.ts
@@ -7,11 +7,12 @@ interface KV {
 export function createAPIGatewayProxyEventV2(
   method: string,
   rawPath: string,
-  { data, headers, pathParameters, queryStringParameters }: {
+  { data, headers, pathParameters, queryStringParameters, isBase64Encoded }: {
     data?: unknown;
     headers?: KV;
     pathParameters?: KV;
     queryStringParameters?: KV;
+    isBase64Encoded?: boolean;
   },
 ): APIGatewayProxyEventV2 {
   const queryString = new URLSearchParams(queryStringParameters).toString();
@@ -19,8 +20,10 @@ export function createAPIGatewayProxyEventV2(
     version: "2",
     routeKey: "",
     headers: headers ?? {},
-    body: data ? JSON.stringify(data) : undefined,
-    isBase64Encoded: false,
+    body: data
+      ? (typeof data === "string" ? data : JSON.stringify(data))
+      : undefined,
+    isBase64Encoded: isBase64Encoded ?? false,
     rawPath: rawPath,
     rawQueryString: queryString,
     requestContext: {
@@ -65,6 +68,28 @@ export function createJSONWebhookEvent(
     data: payload,
     pathParameters,
     queryStringParameters,
+  });
+}
+
+export function createJSONWebhookWebFormEvent(
+  event: string,
+  path: string,
+  payload: unknown,
+  pathParameters: KV,
+  queryStringParameters: KV,
+): APIGatewayProxyEventV2 {
+  return createAPIGatewayProxyEventV2("POST", path, {
+    headers: {
+      "Accept": "*/*",
+      "content-type": "application/x-www-form-urlencoded",
+      "User-Agent": "GitHub-Hookshot/f1aa6e4",
+      "X-GitHub-Delivery": "01b06e5c-d65c-11ea-9409-7e8b4a054eac",
+      "X-GitHub-Event": event,
+    },
+    data: payload,
+    pathParameters,
+    queryStringParameters,
+    isBase64Encoded: true,
   });
 }
 
