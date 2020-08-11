7a81684b3e6e6bfbe246cf8c4cee08487580ef27
Luca Casonato <lucacasonato@yahoo.com>
2020-08-11 17:54:13 +0200

    chore: hide unlisted modules

--- utils/database.ts
+++ utils/database.ts
@@ -10,6 +10,7 @@ export interface Module {
   repository: string;
   description: string;
   star_count: number;
+  is_unlisted: boolean;
 }
 
 export interface SearchResult {
@@ -42,12 +43,8 @@ export interface Build {
 export class Database {
   private mongo = new MongoClient();
   protected db = this.mongo.database("production");
-  _modules = this.db.collection<DBModule>(
-    "modules",
-  );
-  _builds = this.db.collection<Omit<Build, "id"> & { _id: ObjectId }>(
-    "builds",
-  );
+  _modules = this.db.collection<DBModule>("modules");
+  _builds = this.db.collection<Omit<Build, "id"> & { _id: ObjectId }>("builds");
 
   constructor(mongoUri: string) {
     this.mongo.connectWithUri(mongoUri);
@@ -64,6 +61,7 @@ export class Database {
       repository: entry.repository,
       description: entry.description,
       star_count: entry.star_count,
+      is_unlisted: entry.is_unlisted ?? false,
     };
   }
 
@@ -82,6 +80,7 @@ export class Database {
         repository: module.repository,
         description: module.description,
         star_count: module.star_count,
+        is_unlisted: module.is_unlisted,
       },
       { upsert: true },
     );
@@ -140,6 +139,11 @@ export class Database {
 
     //  Query the database
     const docs = (await this._modules.aggregate([
+      {
+        $match: {
+          is_unlisted: { $not: { $eq: true } },
+        },
+      },
       ...searchAggregation,
       {
         $skip: (page - 1) * limit,
@@ -184,13 +188,11 @@ export class Database {
     name: string,
     version: string,
   ): Promise<Build | null> {
-    const build = await this._builds.findOne(
-      {
-        // @ts-expect-error because the deno_mongo typings are incorrect
-        "options.moduleName": name,
-        "options.version": version,
-      },
-    );
+    const build = await this._builds.findOne({
+      // @ts-expect-error because the deno_mongo typings are incorrect
+      "options.moduleName": name,
+      "options.version": version,
+    });
     if (build === null) return null;
     return {
       id: build._id.$oid,
@@ -205,15 +207,13 @@ export class Database {
   async createBuild(
     build: Omit<Omit<Build, "id">, "created_at">,
   ): Promise<string> {
-    const id = await this._builds.insertOne(
-      {
-        created_at: new Date(),
-        options: build.options,
-        status: build.status,
-        message: build.message,
-        stats: build.stats,
-      },
-    );
+    const id = await this._builds.insertOne({
+      created_at: new Date(),
+      options: build.options,
+      status: build.status,
+      message: build.message,
+      stats: build.stats,
+    });
     return id.$oid;
   }
 
