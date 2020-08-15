436570bfda4ce6e01511380193b470f60b9bb93d
Luca Casonato <lucacasonato@yahoo.com>
2020-08-15 09:51:15 +0200

    feat: /module/{name} API endpoint (#66)
    * feat: add /modules/{name} endpoint
    
    * added api.md docs
    
    * Fix module description
    
    * fix tests

---
+++ api/modules/get.ts
@@ -0,0 +1,57 @@
+// Copyright 2020 the Deno authors. All rights reserved. MIT license.
+
+/**
+ * This function is responsible for returning a single module stored in
+ * the database by name. The function is triggered by a HTTP GET call
+ * to /modules/{name}. More information in API.md. 
+ */
+
+import {
+  APIGatewayProxyEventV2,
+  Context,
+  APIGatewayProxyResultV2,
+} from "../../deps.ts";
+import { respondJSON } from "../../utils/http.ts";
+import { Database } from "../../utils/database.ts";
+
+const database = new Database(Deno.env.get("MONGO_URI")!);
+
+export async function handler(
+  event: APIGatewayProxyEventV2,
+  context: Context,
+): Promise<APIGatewayProxyResultV2> {
+  const name = event.pathParameters?.name || undefined;
+
+  if (!name) {
+    return respondJSON({
+      statusCode: 400,
+      body: JSON.stringify({
+        success: false,
+        error: "no module name specified",
+      }),
+    });
+  }
+
+  const module = await database.getModule(name);
+
+  if (module === null) {
+    return respondJSON(
+      {
+        statusCode: 404,
+        body: JSON.stringify({ success: false, error: "module not found" }),
+      },
+    );
+  }
+
+  return respondJSON({
+    statusCode: 200,
+    body: JSON.stringify({
+      success: true,
+      data: {
+        name: module.name,
+        description: module.description,
+        star_count: module.star_count,
+      },
+    }),
+  });
+}
---
+++ api/modules/get_test.ts
@@ -0,0 +1,72 @@
+import { handler } from "./get.ts";
+import {
+  createAPIGatewayProxyEventV2,
+  createContext,
+} from "../../utils/test_utils.ts";
+import { assertEquals } from "../../test_deps.ts";
+import { Database } from "../../utils/database.ts";
+
+const database = new Database(Deno.env.get("MONGO_URI")!);
+
+Deno.test({
+  name: "`/modules/:name`success",
+  async fn() {
+    await database.saveModule({
+      name: "ltest",
+      description: "ltest repo",
+      repository: "luca-rand/testing",
+      star_count: 50,
+      type: "github",
+      is_unlisted: false,
+    });
+
+    const res = await handler(
+      createAPIGatewayProxyEventV2(
+        "GET",
+        "/modules/ltest",
+        { pathParameters: { name: "ltest" } },
+      ),
+      createContext(),
+    );
+
+    assertEquals(
+      res,
+      {
+        body:
+          '{"success":true,"data":{"name":"ltest","description":"ltest repo","star_count":50}}',
+        headers: {
+          "content-type": "application/json",
+        },
+        statusCode: 200,
+      },
+    );
+
+    // Cleanup
+    await database._modules.deleteMany({});
+  },
+});
+
+Deno.test({
+  name: "`/modules/:name`not found",
+  async fn() {
+    const res = await handler(
+      createAPIGatewayProxyEventV2(
+        "GET",
+        "/modules/ltest",
+        { pathParameters: { name: "ltest" } },
+      ),
+      createContext(),
+    );
+
+    assertEquals(
+      res,
+      {
+        body: '{"success":false,"error":"module not found"}',
+        headers: {
+          "content-type": "application/json",
+        },
+        statusCode: 404,
+      },
+    );
+  },
+});
