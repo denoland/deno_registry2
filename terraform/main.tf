220a375e66e00e759b24db848026e33fb8c9ba90
Luca Casonato <lucacasonato@yahoo.com>
2020-08-27 18:15:29 +0200

    fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,10 +34,10 @@ resource "aws_s3_bucket" "moderation_bucket" {
 }
 
 resource "aws_s3_bucket_public_access_block" "moderation_bucket_public_access" {
-  bucket = aws_s3_bucket.moderation_bucket.id
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
+  bucket                  = aws_s3_bucket.moderation_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
