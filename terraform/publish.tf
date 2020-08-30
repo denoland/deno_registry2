051a5de4b22ef81aa362458924e488f4d961e069
William Perron <thewilliamperron@gmail.com>
2020-08-30 19:14:30 -0400

    fix: add required permissions for Lambda to receive messages from SQS (#96)
    Co-authored-by: Luca Casonato <lucacasonato@yahoo.com>

--- terraform/publish.tf
+++ terraform/publish.tf
@@ -33,9 +33,9 @@ resource "aws_lambda_function" "async_publish" {
 
 resource "aws_lambda_event_source_mapping" "async_publish" {
   batch_size       = 1
-  event_source_arn = "${aws_sqs_queue.build_queue.arn}"
+  event_source_arn = aws_sqs_queue.build_queue.arn
   enabled          = true
-  function_name    = "${aws_lambda_function.async_publish.arn}"
+  function_name    = aws_lambda_function.async_publish.arn
 }
 
 resource "aws_lambda_permission" "async_publish" {
