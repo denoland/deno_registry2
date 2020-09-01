2b5f9a7690a0c52aa56de2675493673c90afeb5c
Luca Casonato <lucacasonato@yahoo.com>
2020-09-01 22:42:21 +0200

    Clean up worker

--- terraform/worker.tf
+++ terraform/worker.tf
@@ -1,6 +1,11 @@
 resource "cloudflare_worker_script" "worker" {
   name    = "${local.prefix}-worker-${local.short_uuid}"
-  content = file("../worker/worker.js")
+  content = file("../api/worker.js")
+
+  plain_text_binding {
+    name = "S3_BUCKET"
+    text = "https://${aws_s3_bucket.storage_bucket.website_endpoint}"
+  }
 }
 
 resource "cloudflare_worker_route" "worker_route" {
