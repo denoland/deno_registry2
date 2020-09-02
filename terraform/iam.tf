f9e43000fe155cb304ec69dcfff29c73aa978f8f
Luca Casonato <lucacasonato@yahoo.com>
2020-09-02 20:22:16 +0200

    fix: add listbucket iam permission (#109)

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -20,6 +20,7 @@ data "aws_iam_policy_document" "lambda_permissions" {
       "s3:GetObject",
       "s3:PutObject",
       "s3:PutObjectAcl",
+      "s3:ListBucket",
     ]
     resources = [
       aws_s3_bucket.storage_bucket.arn,
