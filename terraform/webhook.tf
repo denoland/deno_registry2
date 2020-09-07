eba5c05358c926bfceaa4009688f9f9cb5c63a95
William Perron <thewilliamperron@gmail.com>
2020-09-07 13:20:41 -0400

    ops: add publish option to lambda functions (#117)
    This was one of the reasons to switch to Terraform: easily enable Lambda
    versioning using the `publish` option.

--- terraform/webhook.tf
+++ terraform/webhook.tf
@@ -9,6 +9,7 @@ resource "aws_lambda_function" "webhook_github" {
   function_name = "${local.prefix}_webhook_github_${local.short_uuid}"
   role          = aws_iam_role.lambda_exec_role.arn
   handler       = "bundle.handler"
+  publish       = true
 
   source_code_hash = filebase64sha256(data.archive_file.webhook_github_zip.output_path)
 
