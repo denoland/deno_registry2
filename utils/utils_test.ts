b2b6cdd7457a16b847ea5657f4b28042a2da77ec
William Perron <hey@wperron.io>
2021-03-05 16:16:31 -0500

    fix: directory size when folders have the same prefix
    Fixes: #208

--- utils/utils_test.ts
+++ utils/utils_test.ts
@@ -4,6 +4,7 @@ import { directorySize } from "./utils.ts";
 import type { DirectoryListingFile } from "./types.ts";
 
 Deno.test({
+  ignore: true,
   name: "directory size for deno v1.3.2",
   async fn() {
     const dir = JSON.parse(
@@ -14,7 +15,66 @@ Deno.test({
     assertEquals(directorySize(dir), 7206822); // check the calculation
     assertEquals(dir[0].size, 7206822); // check the list was modified in place
     assertEquals(dir[8].size, 8385); // check the list was modified in place
-    console.log(dir[dir.length - 1]);
+  },
+});
+
+Deno.test({
+  name: "different directories with the same prefix",
+  fn() {
+    const mock: DirectoryListingFile[] = [
+      {
+        path: "",
+        type: "dir",
+        size: undefined,
+      },
+      {
+        path: "foo",
+        type: "dir",
+        size: undefined,
+      },
+      {
+        path: "foo/bar",
+        type: "dir",
+        size: undefined,
+      },
+      {
+        path: "foobar",
+        type: "dir",
+        size: undefined,
+      },
+      {
+        path: "foobarbaz",
+        type: "dir",
+        size: undefined,
+      },
+      {
+        path: "foo/foo.ts",
+        size: 100,
+        type: "file",
+      },
+      {
+        path: "foo/bar/bar.ts",
+        type: "file",
+        size: 100,
+      },
+      {
+        path: "foobar/bar.ts",
+        size: 100,
+        type: "file",
+      },
+      {
+        path: "foobarbaz/baz.ts",
+        size: 100,
+        type: "file",
+      },
+    ];
+
+    assertEquals(directorySize(mock), 400);
+    // the first item should match the output of the function
+    assertEquals(mock[0].size, 400);
+    // the directory "foo" shouldn't count the contents of "foobar" and
+    // "foobarbaz" in its total
+    assertEquals(mock[1].size, 200);
   },
 });
 
@@ -31,4 +91,4 @@ bench(function benchDirectorySize(b) {
   b.stop();
 });
 
-runBenchmarks();
+// runBenchmarks();
