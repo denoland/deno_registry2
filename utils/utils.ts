4420b649646636216a0ef9440ece4d00dc7e9cc7
William Perron <hey@wperron.io>
2021-03-08 16:00:42 -0500

    fix: directory size when folders have the same prefix (#210)

--- utils/utils.ts
+++ utils/utils.ts
@@ -28,8 +28,12 @@ export function directorySize(d: DirectoryListingFile[]): number {
 
   let curr = 0;
   for (let i = 1; i < len; i++) { // start at one to skip the root directory
-    while (!d[i].path.startsWith(d[curr].path)) {
-      // current element is out of the curr directory, popping the stack
+    // current element is out of the curr directory, popping the stack
+    //
+    // special case for the root directory; since it's an empty string, it's
+    // easier to simply check for it explicitely than compare the paths with
+    // special conditions to handle "" as the root dir.
+    while (curr != 0 && !d[i].path.startsWith(d[curr].path + "/")) {
       const s = d[curr].size ?? 0;
       curr = stack.pop() as number;
       d[curr].size = (d[curr].size ?? 0) + s;
@@ -38,7 +42,8 @@ export function directorySize(d: DirectoryListingFile[]): number {
     if (d[i].type === "file") {
       totalSize += d[i].size ?? 0;
       d[curr].size = (d[curr].size ?? 0) + (d[i].size ?? 0);
-    } else if (d[i].path.startsWith(d[curr].path)) {
+    } else if (curr === 0 || d[i].path.startsWith(d[curr].path + "/")) {
+      // see comment above for the special case of the root dir
       stack.push(curr);
       curr = i;
     }
