ce7c35b3e10e69856f906c7a1a526600a0375b0d
Christoph Gysin <christoph.gysin@gmail.com>
2021-05-17 22:07:22 +0300

    feat: use git clone filter and sparse checkout (#223)
    Previously, large git repositories could cause the Lambda /tmp to run
    out of space.  This commits introduces  a git clone filter which avoids
    fetching all blobs, and then does a sparse checkout of only the subdir
    (if specified).

--- utils/git.ts
+++ utils/git.ts
@@ -1,17 +1,22 @@
 // Copyright 2020-2021 the Deno authors. All rights reserved. MIT license.
+import { join } from "../deps.ts";
 
-export async function clone(url: string, tag: string): Promise<string> {
+export async function clone(
+  url: string,
+  tag: string,
+  subdir?: string,
+): Promise<string> {
   const tmp = await Deno.makeTempDir();
   const clone = Deno.run({
     cmd: [
       "git",
       "clone",
-      "--depth",
-      "1",
+      "--depth=1",
+      "--filter=blob:none",
+      "--sparse",
       // TODO(lucacasonato): re enable, this is is to slow for the moment
       // "--recursive",
-      "-b",
-      tag,
+      `--branch=${tag}`,
       url,
       tmp,
     ],
@@ -23,6 +28,22 @@ export async function clone(url: string, tag: string): Promise<string> {
   clone.close();
   if (!cloneRes.success) {
     throw new Error(`Failed to clone git repository ${url} at tag ${tag}`);
+  }
+
+  const dir = subdir === undefined ? "/*" : join("/", subdir, "*");
+  const checkout = Deno.run({
+    cwd: tmp,
+    cmd: ["git", "sparse-checkout", "set", dir],
+    stdout: "inherit",
+    stderr: "inherit",
+  });
+  // TODO: better error handling
+  const checkoutRes = await checkout.status();
+  checkout.close();
+  if (!checkoutRes.success) {
+    throw new Error(
+      `Failed to sparse checkout ${dir} from git repository ${url} at tag ${tag}`,
+    );
   }
   return tmp;
 }
