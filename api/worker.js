45216bbeb72c25b8d7c5b7a04356f89574d88dc1
William Perron <hey@wperron.io>
2021-05-24 15:28:31 -0400

    fix fmt

--- api/worker.js
+++ api/worker.js
@@ -3,14 +3,17 @@
 
 /**
  * Handles a request
- * @param {Request} request 
+ * @param {Request} request
  */
 async function handleRequest(request) {
   const url = new URL(request.url);
   // deno-lint-ignore no-undef
-  const resp = await fetch(`${Deno.env.get("S3_BUCKET") ?? ""}${url.pathname}`, {
-    cf: { cacheEverything: true },
-  });
+  const resp = await fetch(
+    `${Deno.env.get("S3_BUCKET") ?? ""}${url.pathname}`,
+    {
+      cf: { cacheEverything: true },
+    },
+  );
   const resp2 = new Response(resp.body, resp);
   resp2.headers.set("Access-Control-Allow-Origin", "*");
   resp2.headers.set(
