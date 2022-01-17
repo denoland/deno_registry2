bb329479a116f58f34176fe5ccf2a1ac548565a8
Luca Casonato <hello@lcas.dev>
2022-01-17 18:57:52 +0100

    fix: update mongo driver to fix atlas compat issue

--- deps.ts
+++ deps.ts
@@ -10,11 +10,14 @@ export type {
   ScheduledEvent,
   SQSEvent,
 } from "https://deno.land/x/lambda@1.17.3/types.d.ts";
-export { Bson, MongoClient } from "https://deno.land/x/mongo@v0.29.0/mod.ts";
+export {
+  Bson,
+  MongoClient,
+} from "https://raw.githubusercontent.com/denodrivers/deno_mongo/09e4c50778786837f6054cff3f87c5836d3b7c65/mod.ts";
 export type {
   Collection as MongoCollection,
   Database as MongoDatabase,
-} from "https://deno.land/x/mongo@v0.29.0/mod.ts";
+} from "https://raw.githubusercontent.com/denodrivers/deno_mongo/09e4c50778786837f6054cff3f87c5836d3b7c65/mod.ts";
 export { S3Bucket } from "https://deno.land/x/s3@0.5.0/mod.ts";
 export { SQSQueue } from "https://deno.land/x/sqs@0.3.7/mod.ts";
 export { SSM } from "https://deno.land/x/ssm@0.1.4/mod.ts";
