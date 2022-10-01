cf53984867f7cfd7241ce75b07cede0e26b206c9
Bert Belder <bertbelder@gmail.com>
2022-10-01 05:58:42 +0200

    fix(terraform): create new certificates before destroying old ones

--- terraform/api.tf
+++ terraform/api.tf
@@ -11,6 +11,10 @@ resource "aws_acm_certificate" "api_certificate" {
   options {
     certificate_transparency_logging_preference = "ENABLED"
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_acm_certificate_validation" "api_certificate_validation" {
