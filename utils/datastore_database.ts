1bf77070bf54c097841df3029e6082ff57fa033d
crowlkats <crowlkats@toaxl.com>
2022-12-15 03:27:04 +0100

    debug

--- utils/datastore_database.ts
+++ utils/datastore_database.ts
@@ -38,6 +38,7 @@ export class Database {
       project_id: Deno.env.get("GOOGLE_PROJECT_ID") ?? "",
       datastore_host: Deno.env.get("DATASTORE_HOST"),
     };
+    console.log(Deno.env.get("DATASTORE_HOST"));
     this.db = new Datastore(keys);
   }
 
