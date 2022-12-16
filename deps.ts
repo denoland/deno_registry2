76f9a29675b55a4cd040f31b846f1cfcb19ddf01
Leo Kettmeir <crowlkats@toaxl.com>
2022-12-16 10:02:37 +0100

    Revert "refactor: use datastore for owner_quota  (#305)"
    This reverts commit 3a37c7bf584a27e7fdfacaecda0a1f20ba9b7844.

--- deps.ts
+++ deps.ts
@@ -21,10 +21,3 @@ export { SSM } from "https://deno.land/x/ssm@0.1.4/mod.ts";
 export { lookup } from "https://deno.land/x/media_types@v2.13.0/mod.ts";
 export { pooledMap } from "https://deno.land/std@0.149.0/async/pool.ts";
 export { readAll } from "https://deno.land/std@0.149.0/streams/conversion.ts";
-export {
-  Datastore,
-  entityToObject,
-  objectGetKey,
-  objectSetKey,
-  objectToEntity,
-} from "https://deno.land/x/google_datastore@0.2.1/mod.ts";
