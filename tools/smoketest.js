e651a3b7090fde60157c21d082a8e1d52b98b2e5
crowlkats <crowlkats@toaxl.com>
2023-01-26 21:39:54 +0100

    fix: remove list from smoketest

--- tools/smoketest.js
+++ tools/smoketest.js
@@ -1,27 +1,7 @@
 // Copyright 2020-2021 the Deno authors. All rights reserved. MIT license.
-const listURI = "https://api.deno.land/modules?page=1&limit=20&query=deno";
 const getURI = "https://api.deno.land/modules/oak";
 
 const errs = [];
-
-const search = fetch(listURI)
-  .then((res) => {
-    if (res.status >= 500) {
-      errs.push(`${listURI} failed with status code ${res.status}`);
-    }
-    return res.json();
-  })
-  .then((res) => {
-    if (!res.success || res.error) {
-      errs.push(`${listURI} failed: ${res.error}`);
-    }
-    if (res.success && res.data?.total_count < 1) {
-      errs.push(`${listURI} expected to return at least one result`);
-    }
-  })
-  .catch((err) => {
-    errs.push(`${listURI} error: ${err}`);
-  });
 
 const get = fetch(getURI)
   .then((res) => {
@@ -42,7 +22,7 @@ const get = fetch(getURI)
     errs.push(`${getURI} error: ${err}`);
   });
 
-await Promise.all([search, get]).then(() => {
+await get.then(() => {
   if (errs.length > 0) {
     console.log(errs.join("\n"));
     Deno.exit(1);
