6077f959d3ef96e84d47b5c86953d2a4fa8a680c
crowlkats <crowlkats@toaxl.com>
2023-05-30 23:20:44 +0200

    refactor: remove builds get endpoint

--- utils/types.ts
+++ utils/types.ts
@@ -1,25 +1,11 @@
 // Copyright 2020-2021 the Deno authors. All rights reserved. MIT license.
 
-import { NewBuild } from "./datastore_database.ts";
-
 export type APIResponseBase = {
   success: boolean;
 };
 
 export type APIErrorResponse = APIResponseBase & { error: string };
 export type APIInfoResponse = APIResponseBase & { info: string };
-
-export type APIStatsResponse = APIResponseBase & {
-  data: {
-    total_count: number;
-    total_versions: number;
-  };
-};
-
-export type APIWebhookResponse =
-  | APIWebhookResponseSuccess
-  | APIInfoResponse
-  | APIErrorResponse;
 
 export type APIWebhookResponseSuccess = APIResponseBase & {
   data: {
@@ -35,16 +21,6 @@ export type APIModuleGetResponse = APIResponseBase & {
     name: string;
     description: string;
     star_count: number;
-  };
-};
-
-export type APIBuildGetResponse =
-  | APIErrorResponse
-  | APIBuildGetResponseSuccess;
-
-export type APIBuildGetResponseSuccess = APIResponseBase & {
-  data: {
-    build: NewBuild;
   };
 };
 
